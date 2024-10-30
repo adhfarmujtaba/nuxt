3b6132b312aa1a87dbb9d151c767f6b322e9b8a4
adhfarmujtaba <adhfarrather@gmail.com>
2024-10-30 18:18:33 +0530

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <transition
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <nuxt />
+    </transition>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      beforeEnter(el) {
+        el.style.opacity = 0;
+      },
+      enter(el, done) {
+        el.offsetHeight; // trigger reflow
+        el.style.transition = 'opacity 0.5s';
+        el.style.opacity = 1;
+        done();
+      },
+      leave(el, done) {
+        el.style.transition = 'opacity 0.5s';
+        el.style.opacity = 0;
+        done();
+      },
+    },
+  };
+  </script>
+  
